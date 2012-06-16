e11f833f13b4c16cf632feb53b5e39b10192dd11
Dody Gunawinata <dodyg@silverkeytech.com>
2012-06-16 17:30:48 +0200

    Add implementation for Bag collection.

--- out/production/Kotlin-Artemis/script.kts
+++ out/production/Kotlin-Artemis/script.kts
@@ -3,7 +3,9 @@ import kotlin.modules.*
 fun project() {
     module("Kotlin-Artemis") {
         sources += "F:/GitHub/GitHub/Kotlin-Artemis/src/artemis/ComponentType.kt"
+        sources += "F:/GitHub/GitHub/Kotlin-Artemis/src/artemis/utils/Bag.kt"
         sources += "F:/GitHub/GitHub/Kotlin-Artemis/src/artemis/Component.kt"
+        sources += "F:/GitHub/GitHub/Kotlin-Artemis/src/artemis/utils/ImmutableBag.kt"
         // Boot classpath
         classpath += "F:/Program Files/Java/jdk1.6.0_21/jre/lib/alt-rt.jar"
         classpath += "F:/Program Files/Java/jdk1.6.0_21/jre/lib/charsets.jar"
